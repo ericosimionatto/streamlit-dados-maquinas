348feed591d24f38c06ecafeb970341f31645e6b
Erico Simionatto <ericosimionatto@gmail.com>
2025-06-13 09:41:14 -0300

    testando comunicação com github

---
+++ Dashboard.py
@@ -0,0 +1,13 @@
+# ----------------------------------------
+# UNOESC Campos de Joaçaba
+# Pós Graduação em Industria 4.0 e IA
+# Disciplçina: Digitalização
+# Professor: Natan Cavasin
+# Trabalho: Implemetar Dashboard com Streamlit
+# Aluno: Érico Simionatto
+# Python, Dataset, Dataframe, Streamlit 
+# 14/06/2025
+# ----------------------------------------
+
+# Importando as bibliotecas necessárias
+import streamlit as st
